80fbb6193ad905c56c70c8310c85e21c7c8302db
patmuchiri <pmuchirig@gmail.com>
2024-07-28 14:18:59 +0300

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -21,8 +21,7 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as e:
-        print(f"Error: {e}")
+    except:
         return None
 
 
@@ -43,8 +42,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
-        print(f"Error: {e}")
+    except:
         return False
 
 
