c98d27807c07c00b02080b1677d163feab90944d
patmuchiri <pmuchirig@gmail.com>
2024-07-29 01:23:24 +0300

    more

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,9 @@
--- prepares a MySQL server for the project
+-- A script that creates the database hbnb_test_db
+-- creates a user and grants priviledges
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
