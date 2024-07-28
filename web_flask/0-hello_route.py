65150d8c09b242a12fb7bdf705f92ade76336ab3
patmuchiri <pmuchirig@gmail.com>
2024-07-29 01:10:05 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
+
+""" Hello Flask! simple flask app """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello():
+    """ route that displays some text """
+    return "Hello HBNB!"
 
 
-if__name__ = '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
